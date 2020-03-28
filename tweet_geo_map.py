71c8015bdf9a7ae52fdda884bd7b5012f59b6554
MrColinHan <changzehan@yahoo.com>
2020-03-28 18:08:05 -0500

    add comments

--- tweet_geo_map.py
+++ tweet_geo_map.py
@@ -6,3 +6,13 @@ import seaborn as sns
 
 sns.set(style=”whitegrid”, palette=”pastel”, color_codes=True)
 sns.mpl.rc(“figure”, figsize=(10,6))
+
+'''
+    problems with the original clean up process and address validation process: 
+        1. not all numbers are invalid, e.g. zip code is also a valid address
+        2. locations written in a different languages could still be US addresses
+        3. locations seem to be wrong could still be correct address
+            e.g. 'YYC' is abbreviation for an airport
+        4. Google geocoding API only allows 2500 free lookups each day. Difficult to use it to check 
+           whether address is a valid US address
+'''
