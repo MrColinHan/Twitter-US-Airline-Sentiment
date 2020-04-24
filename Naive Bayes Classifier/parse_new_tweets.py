b88ce45bbbcb3601545c54886aeb1331b01c92aa
MrColinHan <changzehan@yahoo.com>
2020-04-23 21:13:51 -0500

    regenerate

--- Naive Bayes Classifier/parse_new_tweets.py
+++ Naive Bayes Classifier/parse_new_tweets.py
@@ -14,12 +14,25 @@ import csv
         | tweet_id | airline | name| text | tweet_coord | tweet_created | tweet_location | 
 '''
 # =============================================================================
-input_file_dir = r"/Users/Han/Downloads/web project data/test_data.csv"
-output_file_dir = r"/Users/Han/Downloads/web project data/parsed_tweet_0.csv"
+input_file_dir = r"/Users/Han/Downloads/web project data/tweet_1.csv"
+output_file_dir = r"/Users/Han/Downloads/web project data/parsed_tweet_1.csv"
 
-chunk_size = 50000  # for each data frame
+chunk_size = 500000  # for each data frame
 # =============================================================================
-STRING_MATCHER = ["@Delta", "#Delta", "Delta", "sunset"]  # append more airlines here
+STRING_MATCHER = ["@delta ", "#delta ",
+                  "@unitedairlines", "#unitedairlines", "@united ", "#united ",
+                  "@virginamerica", "#virginamerica",
+                  "@southwestair", "#southwestair",
+                  "@jetblue", "#jetblue",
+                  "@usAirways", "#usAirways",
+                  "@americanair", "#americanair"]  # append more airlines here
+airline_name_dict = {"@delta ": "Delta", "#delta ": "Delta",
+                     "@unitedairlines": "United", "#unitedairlines": "United", "@united ": "United", "#united ": "United",
+                     "@virginamerica": "Virgin America", "#virginamerica": "Virgin America",
+                     "@southwestair": "Southwest", "#southwestair": "Southwest",
+                     "@jetblue": "JetBlue", "#jetblue": "JetBlue",
+                     "@usAirways": "US Airways", "#usAirways": "US Airways",
+                     "@americanair": "American", "#americanair": "American"}  # American, Delta, Southwest, United, US Airways, Virgin America,
 # prepare output data frame
 output_column_names = ["tweet_id", "airline", "name", "text", "tweet_coord", "tweet_created", "tweet_location"]
 output_df = pd.DataFrame(columns=output_column_names)
@@ -44,21 +57,30 @@ def check_string(string):
 
 def main():
     global output_df
+    chunk_count = 1
     for chunk in pd.read_csv(input_file_dir, chunksize=chunk_size):
         #chunk_copy = chunk[chunk.columns[0:7]]  # only keep the first 7 columns
+        #print(chunk)
         for i in chunk.index:  # iterate each row
+            #print(str(chunk["text"][i]))
             for keyword in STRING_MATCHER:
-                if re.search(keyword, chunk["text"][i], re.IGNORECASE):  # check key word
+                if keyword in str(chunk["text"][i]).lower():
+                #if re.search(keyword, chunk["text"][i], re.IGNORECASE):  # check key word
+                    print(str(chunk["text"][i]))
                     output_df = output_df.append({"tweet_id": chunk["tweet_id"][i],
-                                                 "airline": "-TBD-",
+                                                 "airline": airline_name_dict[keyword],
                                                   "name": chunk["name"][i],
                                                   "text": chunk["text"][i],
                                                   "tweet_coord": chunk["tweet_coord"][i],
                                                   "tweet_created": chunk["tweet_created"][i],
-                                                  "tweet_location": chunk["tweet_location"][i]}, ignore_index=True, sort=False)
+                                                  "tweet_location": chunk["tweet_location"][i]},
+                                                 ignore_index=True, sort=False)
+                    break
+        print(f"Done: chunk {chunk_count}: {len(chunk)} tweets")
+        chunk_count += 1
     # write output csv file
     output_df.to_csv(output_file_dir, index=False)
-    print("Done")
+    print(f"Output Written: extracted {len(output_df)} tweets")
 
 
 main()
