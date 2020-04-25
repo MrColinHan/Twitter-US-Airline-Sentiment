34a03c166e4552c5fec890f878e61a2346275a4d
MrColinHan <changzehan@yahoo.com>
2020-04-24 20:39:47 -0500

    add new script: Naive Bayes Classifier for sentiment analysis

---
+++ Naive Bayes Classifier/naive_bayes_classifier.py
@@ -0,0 +1,167 @@
+"""
+@author: Changze Han
+"""
+
+import nltk
+from nltk.tokenize import TweetTokenizer
+from nltk.tag import pos_tag
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+import random
+import csv
+import re
+import string
+from nltk import classify
+from nltk import NaiveBayesClassifier
+
+'''
+    This program performs two tasks: 
+        1. Tokenize text and text stemming
+        2. Train ans Testing the model
+    
+'''
+
+# =============================================================================
+positive_input_file_dir = r"/Users/Han/Downloads/web project data/positive_tweets.csv"
+negative_input_file_dir = r"/Users/Han/Downloads/web project data/negative_tweets.csv"
+text_column_index = 10  # col i starts from 0
+
+output_file_dir = r"/Users/Han/Downloads/web project data/out.csv"
+
+# =============================================================================
+positive_tokens = []
+cleaned_positive_tokens = []
+negative_tokens = []
+cleaned_negative_tokens = []
+
+
+def read_csv(filedir, listname):
+    file = open(filedir)
+    reader = csv.reader(file)
+    for row in reader:
+        listname.append(row)
+
+
+def clean_up_tweets(file_dir, token_list, cleaned_token_list):
+    input_list = []
+    tweets_list = []
+    tweets_list_tokenized = []
+    tweets_list_tokenized_tagged = []
+    tweets_list_tokenized_tagged_lemma = []
+
+    # read tweet texts:
+    read_csv(file_dir, input_list)
+    for row in input_list[1:]:  # exclude header row
+        tweets_list.append(row[text_column_index])
+
+    # tokenize texts:
+    t_tkn = TweetTokenizer()
+    for tweet in tweets_list:
+        tweets_list_tokenized.append(t_tkn.tokenize(tweet))
+        token_list.append(t_tkn.tokenize(tweet))
+
+    # add tags:
+    # NN: noun;  VB: verb
+    for tweet_tokenized in tweets_list_tokenized:
+        tweets_list_tokenized_tagged.append(pos_tag(tweet_tokenized))
+
+    # Lemmatize text:
+    lemmatizer = WordNetLemmatizer()
+    stop_words = stopwords.words('english')
+    for tweet_tokenized_tagged in tweets_list_tokenized_tagged:
+        current_text_lemma = []
+    # remove stop words, hyperlinks, Twitter handles, Punctuation and special characters:
+        for token, tag in tweet_tokenized_tagged:
+            token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|' \
+                           '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
+            token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+            if tag.startswith('NN'):
+                pos = 'n'
+            elif tag.startswith('VB'):
+                pos = 'v'
+            else:
+                pos = 'a'
+            token = lemmatizer.lemmatize(token, pos)
+            if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+                current_text_lemma.append(token.lower())
+        #tweets_list_tokenized_tagged_lemma.append(current_text_lemma)
+        cleaned_token_list.append(current_text_lemma)
+
+    #print(token_list[4])
+    #print(cleaned_token_list[4])
+
+
+# Converting Tokens to a Dictionary: dictionary with words as keys and True as values
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
+
+
+def main():
+    global positive_tokens
+    global cleaned_positive_tokens
+    global negative_tokens
+    global cleaned_negative_tokens
+
+    # get cleaned up tokens
+    print("......Cleaning up Dataset......")
+    print("...tokenizing...")
+    print("...normalizing...")
+    print("...Lemmatizing...")
+    print("...removing stop words...\n")
+    clean_up_tweets(positive_input_file_dir, positive_tokens, cleaned_positive_tokens)
+    print("Done: clean up positive tweets")
+    clean_up_tweets(negative_input_file_dir, negative_tokens, cleaned_negative_tokens)
+    print("Done: clean up negative tweets\n")
+
+    #print(positive_tokens[4])
+    #print(cleaned_positive_tokens[4])
+    #print(negative_tokens[4])
+    #print(cleaned_negative_tokens[4])
+
+    # Converting Tokens to a Dictionary:
+    positive_tokens_for_model = get_tweets_for_model(cleaned_positive_tokens)
+    negative_tokens_for_model = get_tweets_for_model(cleaned_negative_tokens)
+    print("Done: Convert tokens to dictionaries.\n")
+
+    # create a dataset by joining the positive and negative tweets.
+    positive_dataset = [(tweet_dict, "Positive")
+                        for tweet_dict in positive_tokens_for_model]
+    negative_dataset = [(tweet_dict, "Negative")
+                        for tweet_dict in negative_tokens_for_model]
+    dataset = positive_dataset + negative_dataset
+    print("Done: Combine dataset by joining the positive and negative tweets.")
+    # random shuffle
+    random.shuffle(dataset)
+
+    print(f"negative dataset: {len(positive_dataset)} tweets.")
+    print(f"positive dataset: {len(negative_dataset)} tweets.")
+    print(f"combine positive & negative dataset: {len(dataset)} tweets.\n")
+    print("......Training Data......")
+
+    # splits the shuffled data into a ratio of 7:3 for training and testing
+    train_data = dataset[:round(len(dataset)*0.7)]
+    test_data = dataset[round(len(dataset)*0.7):]
+    print(f"train data: {len(train_data)} tweets")
+    print(f"test data: {len(test_data)} tweets\n")
+
+    print("Build & Test Naive_Bayes_Classifier Model: ")
+    classifier = NaiveBayesClassifier.train(train_data)
+
+    print(f"Accuracy is:{classify.accuracy(classifier, test_data)}\n")
+
+    print(classifier.show_most_informative_features(10))
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
