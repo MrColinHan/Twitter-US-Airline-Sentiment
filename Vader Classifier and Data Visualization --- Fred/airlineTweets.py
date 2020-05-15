f8e879b53f930d74d5ab2be493a883edd1d40148
MrColinHan <changzehan@yahoo.com>
2020-05-15 02:24:08 -0500

    update

--- Vader Classifier and Data Visualization --- Fred/airlineTweets.py
+++ Vader Classifier and Data Visualization --- Fred/airlineTweets.py
@@ -7,10 +7,6 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 # Read in tweets dataset
 tweets = pd.read_csv("finalInput.csv")
 
-# Use text to predict airline_sentiment_gold (NLTK)
-# Generate word cloud for positive and negative words using text
-# 
-
 #--------------------------------------------------------------------------------------------
 # Question: Which airlines get tweeted about the most?
 # Bar graph with numbers
@@ -23,7 +19,7 @@ print(numberOfTweetsPerAirline)
 print()
 
 # Using Pie chart to plot frequency of tweets per airline
-words = [('United', 4648), ('US Airways', 2913), ('American', 3450), ('Southwest', 2814), ('Delta', 3052), ('Virgin America', 568)]
+words = [('United', 4648), ('US Airways', 2913), ('American', 3450), ('Southwest', 2814), ('Delta', 3052), ('Virgin America', 568), ('JetBlue', 238)]
 sizes, labels = [i[1] for i in words],[i[0] for i in words]
 plt.title("Frequency of Tweets Per Airline")
 plt.pie(sizes, labels=labels,autopct='%1.1i%%')
@@ -63,32 +59,6 @@ plt.bar(reasons, topTenNegativeReasons, color=colors)
 plt.xticks(fontsize=7)
 plt.show()
 
-
-"""
-#--------------------------------------------------------------------------------------------
-# Best Airlines by Number of Positive Reasons (airline_sentiment)
-# Pie chart with frequencies
-# Data Frame
-#--------------------------------------------------------------------------------------------
-print("Best Airlines by Number of Positive Reasons (airline_sentiment)")
-numberOfTweetsBySentiment = tweets.airline_sentiment.value_counts()
-is_positive = tweets.airline_sentiment.str.contains("positive")
-positive_tweets = tweets[is_positive]
-positive_tweets.shape
-best_airline = positive_tweets[['airline','airline_sentiment_confidence']]
-#best_airline = (best_airline.groupby('airline').airline_sentiment_confidence.count()/len(best_airline)).sort_values(ascending=False)
-best_airline = best_airline.groupby('airline').airline_sentiment_confidence.count().sort_values(ascending=False)
-print(best_airline)
-print()
-"""
-# Pie chart Best Airlines by Number of Positive Reasons
-words = [('Southwest', 570), ('Delta', 544), ('United', 492), ('American', 336), ('US Airways', 269), ('Virgin America', 152)]
-sizes, labels = [i[1] for i in words],[i[0] for i in words]
-plt.title("Best Airlines by Number of Positive Reasons (airline_sentiment)")
-plt.pie(sizes, labels=labels,autopct='%1.1i%%')
-plt.show()
-
-
 #--------------------------------------------------------------------------------------------
 # Worst Airlines by Number of Negative Reasons (negativereason)
 # Pie chart with frequencies
@@ -103,7 +73,7 @@ worst_airline = worst_airline.groupby('airline').negativereason.count().sort_val
 print(worst_airline)
 
 # Pie chart for Worst Airlines by Number of Negative Reasons (negativereason)
-words = [('United', 3301), ('US Airways', 2263), ('American', 2518), ('Southwest', 1440), ('Delta', 1588), ('Virgin America', 230)]
+words = [('United', 3301), ('US Airways', 2263), ('American', 2518), ('Southwest', 1440), ('Delta', 1588), ('Virgin America', 230), ('JetBlue', 163)]
 sizes, labels = [i[1] for i in words],[i[0] for i in words]
 plt.title("Worst Airlines by Number of Negative Reasons (negativereason)")
 plt.pie(sizes, labels=labels,autopct='%1.1i%%')
@@ -131,7 +101,7 @@ plt.legend(bbox_to_anchor=(0.95, 0.7))
 plt.show()
 
 
-######### Machine Learning part for airline_sentiment_gold #############
+######### Machine Learning part for sentiment #############
 analyser = SentimentIntensityAnalyzer()
 
 # Columns needed for classification 
@@ -144,16 +114,15 @@ print("Number of Tweets is: " + str(len(tweets)))
 
 ########## Compute VADER scores ###################
 
-i=0 #counter
-compval1 = []  #empty list to hold our computed 'compound' VADER scores
+i=0 
+compval1 = []  
 while (i<len(tweets)):
     k = analyser.polarity_scores(tweets.iloc[i]['text'])
     compval1.append(k['compound'])
     i = i+1
     
-#converting sentiment values to numpy for easier usage
+
 compval1 = np.array(compval1)
-#len(compval1)
 
 # Add new column called VADER score with computed values
 tweets['VADER Score'] = compval1
@@ -161,10 +130,9 @@ tweets['VADER Score'] = compval1
 outfile = open('newTweets.csv', 'w')
 tweets.to_csv(outfile, index=False)
 
-####### Using VADER Scores for airline_sentiment_gold column ########
+####### Using VADER Scores for VADER Score column #################
 i = 0
 
-#empty series to hold our predicted values
 predicted_value = [] 
 
 # Use VADER scores to determine sentiment
@@ -186,8 +154,7 @@ outfile = open('finalOutput.csv', 'w')
 tweets.to_csv(outfile, index=False)
 outfile.close()
 
-################### Prediction Accuracy, comparing difference between airline_sentiment_gold
-# and airline_sentiment
+################### Prediction Accuracy #####################
 
 correctPredictions = tweets[tweets['airline_sentiment'] == tweets['vader_airline_sentiment']]
 print("Percentage of correct predictions: " + str(len(correctPredictions)/len(tweets)))
